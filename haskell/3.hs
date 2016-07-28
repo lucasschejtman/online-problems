745eeda44b1b5efceb970acc82b9dde8ae3ef197
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-07-28 14:52:29 +1200

    nicer syntax

--- haskell/3.hs
+++ haskell/3.hs
@@ -2,9 +2,9 @@
 	The prime factors of 13195 are 5, 7, 13 and 29.
 	What is the largest prime factor of the number 600851475143 ?
 -}
-
+--(\ x -> rem x n /= 0)
 primes :: [Int]
-primes = primes' [2..] where primes' (n:ns) = n:(primes' $ filter (\ x -> rem x n /= 0) ns)
+primes = primes' [2..] where primes' (n:ns) = n:(primes' $ filter ((/= 0) . (rem n)) ns)
 
 solution :: Int -> [Int]
 solution n = factors n primes

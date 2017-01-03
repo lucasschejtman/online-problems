061fc059da777185e4e24d2bc1b0a05483866186
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-01-04 08:54:08 +1300

    remove unnecessary code

--- hacker rank/functional/recursion/3_pascal.hs
+++ hacker rank/functional/recursion/3_pascal.hs
@@ -7,12 +7,6 @@ coordinate n r = fac(n) `div` (fac(r) * fac(n - r))
 triangle :: [[Int]]
 triangle = [ [ coordinate n r | r <- [0..n] ] | n <- [0..] ]
 
-two :: [Int] -> [String]
-two m = map (show) m
-
-three :: [[Int]] -> [[String]]
-three m = map (two) m
-
 main = do
    m <- readLn :: IO Int
    mapM_ (putStrLn . unwords) $ map (map show) $ (take m $ triangle)
